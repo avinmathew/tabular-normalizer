1811896b83320d19fa09c47b792a58246341e303
Avin Mathew <651804+avinmathew@users.noreply.github.com>
2020-12-23 15:03:46 +1000

    Check if there are objects before trying to process

--- tabular-normalizer.js
+++ tabular-normalizer.js
@@ -11,32 +11,38 @@ const inFile = fs.readFileSync(inputFile);
 const model = JSON.parse(inFile);
 
 // Reorder tables
-model.model.tables.sort((a, b) => a.name.localeCompare(b.name));
+if (model.model.tables) {
+    model.model.tables.sort((a, b) => a.name.localeCompare(b.name));
 
-model.model.tables.forEach(table => {
-    // Reorder columns
-    table.columns.sort((a, b) => a.name.localeCompare(b.name));
+    model.model.tables.forEach(table => {
+        // Reorder columns
+        table.columns.sort((a, b) => a.name.localeCompare(b.name));
 
-    // Reorder measures
-    if (table.measures) {
-        table.measures.sort((a, b) => a.name.localeCompare(b.name));
-    }
-});
+        // Reorder measures
+        if (table.measures) {
+            table.measures.sort((a, b) => a.name.localeCompare(b.name));
+        }
+    });
+}
 
 // Reorder relationships
-model.model.relationships.sort((a, b) => {
-    return (a.fromTable + a.toTable + a.fromColumn + a.toColumn).localeCompare(b.fromTable + b.toTable + b.fromColumn + b.toColumn);
-});
+if (model.model.relationships) {
+    model.model.relationships.sort((a, b) => {
+        return (a.fromTable + a.toTable + a.fromColumn + a.toColumn).localeCompare(b.fromTable + b.toTable + b.fromColumn + b.toColumn);
+    });
+}
 
 // Reorder roles
-model.model.roles.sort((a, b) => a.name.localeCompare(b.name));
+if (model.model.roles) {
+    model.model.roles.sort((a, b) => a.name.localeCompare(b.name));
 
-model.model.roles.forEach(role => {
-    // Reorder table permissions
-    if (role.tablePermissions) {
-        role.tablePermissions.sort((a, b) => a.name.localeCompare(b.name));
-    }
-});
+    model.model.roles.forEach(role => {
+        // Reorder table permissions
+        if (role.tablePermissions) {
+            role.tablePermissions.sort((a, b) => a.name.localeCompare(b.name));
+        }
+    });
+}
 
 const outFile = JSON.stringify(model, null, 2); // spacing level = 2 to match original
 fs.writeFileSync(outputFile, outFile);
